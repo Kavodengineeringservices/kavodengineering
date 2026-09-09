97af17c7618e9d36731128aaf4e15499bef0c93c
Jesse Adesina <jesseoluwapelumi01@gmail.com>
2026-09-09 12:03:22 +0100

    feat: remove fixed contact banner from website top

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import "../styles/app.css";
 import { Navbar } from "@/components/layout/navbar";
 import { Footer } from "@/components/layout/footer";
-import { ContactBanner } from "@/components/layout/contactBanner";
 import { Toaster } from "sonner";
 
 export const metadata: Metadata = {
@@ -38,7 +37,6 @@ export default function RootLayout({
     <html lang="en">
       <body className="antialiased overscroll-none">
         <header className="w-full fixed top-0 right-0 left-0 z-50">
-          <ContactBanner />
           <Navbar />
         </header>
         {children}
